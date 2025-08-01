2aacee686a95830dcec50c8e3ccabe5a321ade1d
isax7 <isaccostiaccini@gmail.com>
2025-08-01 17:43:18 +0200

    paths+

--- py/Paths.py
+++ py/Paths.py
@@ -1,2 +1,7 @@
-def unique_paths():
-    pass
+def unique_paths(m:int, n:int):
+    dp = [[0 for _ in range(n+1)] for __ in range(m+1)]
+    dp[0][1] = 1
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            dp[i][j] = dp[i][j-1] + dp[i-1][j]
+    return dp[m][n]
